c167246f9c7af5ca25a05cc0a5fcb53a990b37fc
Kim hanjae <samseong0418@naver.com>
2024-07-17 09:51:27 +0900

    20240717

---
+++ for문 기초 prac1.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    if i % 2 == 0:
+        print('#*#*' * i)
+        
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in nums:
+    print(i, '번', end = ' ')
---
+++ 문풀/10871번 X보다 작은 수.py
@@ -0,0 +1,9 @@
+# 10871번 X보다 작은 수
+N, X = map(int, input().split())  # N만큼 A 갯수를 일단 받을 거임, X는 기준이 되는 수
+A = list(map(int, input().split()))  # A 리스트에 N만큼 숫자를 넣장
+
+if N > 0 and X > 0 and N <= 10000 and X <= 10000:  # N과 X의 조건에 따라 범위 정함
+    for i in range(N):  # N번 반복해서
+        if A[i] < X:  # A 리스트의 요소 중에서 i번째 수가 X보다 작으면?
+            print(A[i], end = ' ')  # 그 요소를 출력할거구 출력하고 한칸씩 띄어쓸거임.
+
