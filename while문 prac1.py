557428eacc2c6273f483e61345a042f7745edf4a
Kim hanjae <samseong0418@naver.com>
2024-07-18 11:26:59 +0900

    20240718

---
+++ while문 prac1.py
@@ -0,0 +1,8 @@
+i = 1
+sum = 0
+while True:
+    sum = sum + i
+    if i == 100:
+        break
+    i = i + 1
+print(sum)
---
+++ 문풀/9086번 문자열.py
@@ -0,0 +1,6 @@
+# 9086번 문자열
+
+T = int(input())
+for i in range(T):  # T만큼 반복
+    word = str(input()) # 문자열 T번만큼 입력
+    print(word[0]+word[-1]) # 각각 앞글자와 뒷글자 출력
