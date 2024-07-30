99236f3080932f69a096bff09cb1c5a97ccf052b
DarkLightBegin23 <samseong0418@naver.com>
2024-07-30 11:27:12 +0900

    20240730

---
+++ 문제풀이/125 - 300제 문제.py
@@ -0,0 +1,15 @@
+# 125 - 300제 문제
+telephone_num = input("휴대전화 번호 입력 :")
+
+telephone = ['SKT', 'KT', 'LGU', '기타']
+
+if telephone_num [2]== '1':
+    print(f'당신은 {telephone[0]} 사용자입니다.')
+elif telephone_num[2] == '6':
+    print(f'당신은 {telephone[1]} 사용자입니다.')
+elif telephone_num[2] == '9':
+    print(f'당신은 {telephone[2]} 사용자입니다.')
+elif telephone_num[2] == '0':
+    print(f'당신은 {telephone[3]} 사용자입니다.')
+else:
+    print("없는 번호입니다.")
