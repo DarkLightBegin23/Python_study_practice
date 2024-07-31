7bd50a6219da24baf72b4039c7e1dffaa6334425
DarkLightBegin23 <samseong0418@naver.com>
2024-07-31 09:32:34 +0900

    20240731

---
+++ 문제풀이/254 - 300제 문제(review_nec).py
@@ -0,0 +1,7 @@
+# 254 - 300제 문제
+
+class Human:
+    def __init__(self):
+        print("응애응애")
+    
+areum = Human()
---
+++ 문제풀이/2935번 소음.py
@@ -0,0 +1,12 @@
+# 2935번 소음
+
+A = int(input())
+cal = input()
+B = int(input())
+
+if cal == '+':
+    print(A + B)
+if cal == '*':
+    print(A * B)
+else:
+    pass
